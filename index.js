735f92ba56cd9aa299525defa4c941e69ad6acd7
Robert Loo <rloolee89@gmail.com>
2022-09-24 18:01:43 -0400

    Create json structure for add recipe feature

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const app = express();
 const PORT = process.env.PORT;
 // const video = require("./routes/videos");
 
-
 const cors = require("cors");
 app.use(express.json());
+
+
+
+// This middleware is a basic example that runs on every request
+app.use((req, res, next) => {
+    console.log("Incoming request");
+    next();
+  });
+
 
 app.listen(PORT, () => {
     console.log("App has started at port " + PORT);
