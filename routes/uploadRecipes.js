d2ade57554b2743efea953561c78429f8bd13bd4
Robert Loo <rloolee89@gmail.com>
2022-09-25 00:24:59 -0400

    Finish creating Upload Recipes route

--- routes/uploadRecipes.js
+++ routes/uploadRecipes.js
@@ -2,3 +2,41 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const crypto = require("crypto");
+
+function readRecipes(){
+    const recipesFile =fs.readFileSync("./data/recipes.json");
+    const videoData = JSON.parse(recipesFile);
+    return videoData
+}
+
+
+
+router.get("/", (req, res) => {
+    const recipes = readRecipes();
+    res.json(recipes);
+});
+
+
+router.post("/", (req, res) => {
+    // Make a new recipe post
+    const newRecipe = {
+      id: crypto.randomUUID(),
+      image: req.body.image,
+      recipetitle: req.body.reciptitle,
+      titleintroduction: req.body.recipeintroduction,
+      ingredients: req.body.ingredients,
+      howto: req.body.howto,
+
+    };
+
+   // 1. Read the current recipes array
+    // 2. Add to the recipes array
+    // 3. Write the entire new recipes array to the file
+    const recipes = readRecipes();
+    videos.push(newRecipe);
+    fs.writeFileSync("./data/recipes.json", JSON.stringify(recipes));
+    res.status(201).json(newRecipe);
+});
+
+
+module.exports = router;
